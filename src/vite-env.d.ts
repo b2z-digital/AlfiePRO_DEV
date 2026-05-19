a6ed0355e92217a0150309c52a8c0d65d24e1c05
b2z-digital <30684594+b2z-digital@users.noreply.github.com>
2026-05-19 14:58:31 +1000

    Updated vite.config.ts

--- src/vite-env.d.ts
+++ src/vite-env.d.ts
@@ -1,5 +1,8 @@
 /// <reference types="vite/client" />
 
+declare const __APP_BUILD_DATE__: string;
+declare const __APP_BUILD_TIME__: string;
+
 // Add Google Maps global declaration
 declare global {
   interface Window {
--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,10 @@ import path from "path"
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  define: {
+    __APP_BUILD_DATE__: JSON.stringify(new Date().toISOString().slice(0, 10).replace(/-/g, '')),
+    __APP_BUILD_TIME__: JSON.stringify(new Date().toISOString()),
+  },
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src")
